b0fa71cf0aba428a78027ee2a4fd19fda5267f8e
Kevin <cubs1234@comcast.net>
2019-07-23 14:49:52 -0500

    data

---
+++ app/data/friends.js
@@ -0,0 +1,79 @@
+var friends = [
+
+    {
+
+        name: "Gomer Pyle",
+
+        photo:
+
+            "https://images.app.goo.gl/yP3N4USMxosiL4Vh7",
+
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+
+    },
+
+    {
+
+        name: "Albert Einstein",
+
+        photo:
+
+            "https://images.app.goo.gl/CcjFk5FqsbKVzHS46",
+
+        scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+
+    },
+
+    {
+
+        name: "nikola tesla",
+
+        photo:
+
+            "https://images.app.goo.gl/i8T5FGxbQs76N8xi8",
+
+        scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+
+    },
+
+    {
+
+
+
+        name: "jane addams",
+
+        photo:
+
+            "https://images.app.goo.gl/SjW27DzpbRs6kGgy9",
+
+        scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+
+    },
+
+    {
+
+        name: "lady bird johnson",
+
+        photo:
+
+            "https://images.app.goo.gl/4mAsTegttmjZLsRF6",
+
+        scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+
+    },
+
+    {
+
+        name: "martha tilton",
+
+        photo:
+
+            "https://images.app.goo.gl/jeXrrP4veJf3LVPa7",
+
+        scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+
+    }
+
+];
+
+module.exports = friends;
