2aa53c3ffd3aea56d7cc2e8671479099c36f85c0
Kevin <cubs1234@comcast.net>
2019-07-23 14:49:09 -0500

    server and package

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+    type: "application/vnd.api+json"
+}));
+
+app.use(express.static("app/public"));
+
+require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
+
+
+
+app.listen(port, () => {
+    return console.log("Listening on port %s", port);
+});
